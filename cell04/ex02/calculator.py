aa7952ae37ab927793363e5519fb384b510e7a2c
kwankhao6995 <kwankanyapak3@gmail.com>
2025-06-18 02:48:51 +0000

    300

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,11 @@
+num_1 = int(input("Give me the first number: "))
+num_2 = int(input("Give me the second number: "))
+print('Thank You')
+print(f"{num_1} + {num_2} = {num_1 + num_2}")
+print(f"{num_1} - {num_2} = {num_1 - num_2}")
+div = num_1 / num_2
+if div == int(div):
+print(f"{num1} / {num2} = {int(num1 / num2)}")
+else:
+print(f"{num_1} / {num_2} = {div}")
+print(f"{num_1} * {num_2} = {num_1 * num_2}")
